f302c394136b45333587bbb65c37adac4766aba6
Felipe Goriano <Felipe Goriano@192.168.1.19>
2023-05-19 22:58:56 -0300

    Commit inicial

---
+++ estudo-observability/src/main/java/br/com/felipe/poc/estudoobservability/EstudoObservabilityApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.poc.estudoobservability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoObservabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoObservabilityApplication.class, args);
+	}
+
+}
