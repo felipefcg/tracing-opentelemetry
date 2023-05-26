eabd66a5456307f3838cd445a0ca27893e339f2c
Felipe Goriano <Felipe Goriano@192.168.1.19>
2023-05-26 14:36:40 -0300

    Integração Micrometer com Spring Web MVC/Flux
    Integração do Micrometer com o Spring MVC e Web Flux
    
    Para MVC e WebFlux, existem filtros que podem ser usados ​​para
    observações do servidor HTTP:
    
    	* org.springframework.web.filter.ServerHttpObservationFilter para
    Spring MVC
    	* org.springframework.web.filter.reactive.ServerHttpObservationFilter
    para WebFlux
    Quando o Actuator faz parte do nosso aplicativo, esses filtros já estão
    cadastrados e ativos. Se não, precisamos configurá-lo conforme classe
    ObservationFilterConfiguration.

--- estudo-observability/src/main/java/br/com/felipe/poc/estudoobservability/config/ObservationFilterConfiguration.java
+++ estudo-observability/src/main/java/br/com/felipe/poc/estudoobservability/config/ObservationFilterConfiguration.java
@@ -8,13 +8,27 @@ import org.springframework.web.filter.ServerHttpObservationFilter;
 
 import io.micrometer.observation.ObservationRegistry;
 
-//@Configuration
+/**
+ * 
+ * @author Felipe Goriano
+ * @link https://www.baeldung.com/spring-boot-3-observability
+ * Essa classe foi criado para integrar o Micrometer com o Spring Actuator, utilizando como documentação o item 3.2  do artigo <b>Observabilidade com Spring Boot 3</b>.
+ * Para MVC e WebFlux, existem filtros que podem ser usados ​​para observações do servidor HTTP:
+ * 	<ul>
+ * 		<li>org.springframework.web.filter.ServerHttpObservationFilter para Spring MVC</li>
+ * 		<li>org.springframework.web.filter.reactive.ServerHttpObservationFilter para WebFlux</li>
+ * <ul>
+ * Quando o Actuator faz parte do nosso aplicativo, esses filtros já estão cadastrados e ativos. Se não, precisamos configurá-los, conforme essa classe
+ * Podemos encontrar mais detalhes sobre a integração de observabilidade no Spring Web na <a href="https://www.baeldung.com/spring-boot-3-observability">documentação</a>
+ */
+
+@Configuration
 public class ObservationFilterConfiguration {
 
-//	@ConditionalOnBean(ObservationRegistry.class)
-//	@ConditionalOnMissingBean(ServerHttpObservationFilter.class)
-//	@Bean
-//	public ServerHttpObservationFilter observationFilter (ObservationRegistry registry) {
-//		return new ServerHttpObservationFilter(registry);
-//	}
+	@ConditionalOnBean(ObservationRegistry.class)
+	@ConditionalOnMissingBean(ServerHttpObservationFilter.class)
+	@Bean
+	public ServerHttpObservationFilter observationFilter (ObservationRegistry registry) {
+		return new ServerHttpObservationFilter(registry);
+	}
 }
